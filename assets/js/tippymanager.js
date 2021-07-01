b390cec83ec8245e1b3d4c10b666f011e08a15af
mikol <mikol.aspinwall@gmail.com>
2021-06-30 22:34:29 -0400

    final touches

--- assets/js/tippymanager.js
+++ assets/js/tippymanager.js
@@ -1,15 +1,18 @@
 const profileCard = document.querySelector('#profileCardTemplate .profile');
 const developerCard = document.querySelector('#developerTemplate .dev-profile');
 const developerDetails = document.querySelector('#developerDetails .language-details');
+const workedDropdown = document.querySelector('#workedDropdown .options');
+const workedDropdownBelow = document.querySelector('#workedDropdownBelow .options');
+
 
 tippy('.dev-profile', {
     allowHTML: true,
-    interactive: true,
+    interactive: false,
     content: developerDetails,
     arrow: false,
     triggerTarget: [...document.querySelectorAll('.dev-profile__body img')],
     placement: 'top',
-    interactiveBorder: 40
+    interactiveBorder: 40,
 })
 
 tippy('.profile', {
@@ -21,7 +24,13 @@ tippy('.profile', {
     placement: 'top',
     maxWidth: '550',
     interactiveBorder: 40,
-    sticky: 'reference'
+    sticky: 'reference',
+    onTrigger(instance) {
+        instance.props.triggerTarget.classList.add('selected');
+    },
+    onUntrigger(instance) {
+        instance.props.triggerTarget.classList.remove('selected');
+    }
 });
 
 tippy('#name', {
@@ -31,5 +40,29 @@ tippy('#name', {
     arrow: false,
     animation: 'shift-away-extreme',
     interactiveBorder: 40,
-    sticky: 'reference'
+    sticky: 'reference',
+    onTrigger(instance) {
+        instance.reference.classList.add('selected');
+    },
+    onUntrigger(instance) {
+        instance.reference.classList.remove('selected');
+    }
 });
+
+tippy('.headline', {
+    allowHTML: true,
+    interactive: true,
+    content: `<h2><a href="mailto:mikol.aspinwall@gmail.com">Email me. Let's create something awesome.</a></h2>`,
+    arrow: false,
+    interactiveBorder: 40,
+    sticky: 'reference',
+    triggerTarget: document.querySelector('#work'),
+    placement: 'bottom',
+    onTrigger(instance) {
+        instance.props.triggerTarget.classList.add('selected');
+    },
+    onUntrigger(instance) {
+        instance.props.triggerTarget.classList.remove('selected');
+    },
+    maxWidth: 'none'
+})
